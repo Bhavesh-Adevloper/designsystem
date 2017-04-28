1547236527e8c8f9c286c2d1e4bb27af6ce112b1
Daniel Strietzel <daniel.strietzel@bouvet.no>
2017-04-28 10:24:58 +0200

    update

--- js/fixPatternLinks.js
+++ js/fixPatternLinks.js
@@ -1,5 +1,6 @@
 /* globals $ */
-if (window.location.pathname.indexOf('designsystem') === 1) {
+var fixPatternLinks = function() {
+  if (window.location.pathname.indexOf('designsystem') === 1) {
     $('a').each(function() {
       if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('designsystem') === -1) {
         $(this).attr('href', $(this).attr('href').replace('/patterns/', '/designsystem/patterns/'));
@@ -10,4 +11,9 @@ if (window.location.pathname.indexOf('designsystem') === 1) {
         $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/designsystem/patterns/'));
       }
     });
-}
+  }
+};
+
+$(document).ready(function () {
+    fixPatternLinks();
+});
