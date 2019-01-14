5582073599aeb4c5d308b4416559a27612b55d13
Daniel Strietzel <daniel.strietzel@bouvet.no>
2019-01-14 15:12:42 +0100

    update

--- js/portalen-functions.js
+++ js/portalen-functions.js
@@ -56,7 +56,8 @@ function addClassOnExternalLinks() {
 			if (url.match(externalLink)) {
 				if (! $(this).html().match(/<img/i)) {
     				if(!(
-        				$(this).hasClass('service_shortcut_icon_link') // Feilmelding
+        				$(this).hasClass('js_removeExternalLinkIcon') // Generell
+        				|| $(this).hasClass('service_shortcut_icon_link') // Feilmelding
         				|| $(this).hasClass('some_footer_link') // Footer SoMe
         				|| $(this).hasClass('intern_link') // Ansattpålogging
         				|| $(this).hasClass('contact_static_image_link') // GoogleMaps
